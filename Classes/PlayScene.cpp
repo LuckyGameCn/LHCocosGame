701221fee16420319532ce31a3a0e24072fe9779
Luckyhu <huji0624@gmail.com>
2014-06-18 19:27:31 +0800

    ok.
    FYI.

--- Classes/PlayScene.cpp
+++ Classes/PlayScene.cpp
@@ -50,34 +50,6 @@ bool PlayScene::init()
     this->addChild(label,1);
     scoreLabel = label;
     
-//    Sprite *collect = Sprite::create("play.png", Rect(0, 0, visibleSize.width/5, visibleSize.height/10));
-//    collect->setPosition(visibleSize.width/2, collect->getContentSize().height/2);
-//    this->addChild(collect, 1);
-//    auto ls = EventListenerTouchOneByOne::create();
-//    // 设置是否吞没事件，在 onTouchBegan 方法返回 true 时吞没
-//    ls->setSwallowTouches(true);
-//    
-//    ls->onTouchBegan =  [this](Touch* touch, Event* event){
-//        return true;
-//    };
-//    ls->onTouchEnded =  [this](Touch* touch, Event* event){
-//        log("collect");
-//        if (this->firsttouch) {
-//            return;
-//        }
-//        int touches[TRCOUNT];
-//        int ii = 0;
-//        for (int i = 0 ; i<TRCOUNT; i++) {
-//            if (touchrecord[i] == 1) {
-//                touches[ii] = i;
-//                ii ++;
-//            }
-//        }
-//        this->blockClick(touches,lineCount);
-//        memset(touchrecord, -1, TRCOUNT);
-//    };
-//    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(ls, collect);
-    
     auto hardItem = MenuItemImage::create("play.png", "play.png", CC_CALLBACK_1(PlayScene::collect, this));
     hardItem->setPosition(visibleSize.width/2, hardItem->getContentSize().height/2);
     
@@ -90,11 +62,9 @@ bool PlayScene::init()
 }
 
 void PlayScene::collect(cocos2d::Ref *pSender){
-    int touches[TRCOUNT];
     int ii = 0;
     for (int i = 0 ; i<TRCOUNT; i++) {
         if (touchrecord[i] == 1) {
-            touches[ii] = i;
             ii ++;
         }
     }
@@ -102,7 +72,7 @@ void PlayScene::collect(cocos2d::Ref *pSender){
         memset(touchrecord, -1, TRCOUNT*sizeof(int));
         return;
     }else{
-        this->blockClick(touches,ii);
+        this->blockClick(touchrecord,lineCount);
         memset(touchrecord, -1, TRCOUNT*sizeof(int));
     }
 }
@@ -125,9 +95,10 @@ void PlayScene::genBlockLine(){
     float width = linewidth - hmargin*2;
     float heigt = visibleSize.height/10;
     
-    //    Vector<Sprite*> blockline(lineCount);
-    CCArray *blockline = CCArray::create();
+//    Vector<Sprite*> blockline(lineCount);
+//    CCArray *blockline = CCArray::create();
     for (int i = 0 ; i<lineCount; i++) {
+        CCArray *blockline = (CCArray*) array->objectAtIndex(i);
         int tag = (int)(CCRANDOM_0_1() * lineCount);
         float x = i*linewidth + linewidth/2;
         char fnc[10];
@@ -138,11 +109,8 @@ void PlayScene::genBlockLine(){
         block->setPosition(x, y);
         block->setTag(tag);
         this->addChild(block, 0);
-//        this->addChild(block);
-        //        blockline.pushBack(block);
         blockline->addObject(block);
     }
-    array->addObject(blockline);
 }
 
 //static float bottomHeight = 60;
@@ -155,13 +123,12 @@ void PlayScene::update(float dt){
             float x = block->getPosition().x;
             float y = block->getPosition().y;
             block->setPosition(x, y-dt*speed);
+            
+            if (block->getPosition().y<=block->getContentSize().height/2) {
+                this->gameOver();
+                return;
+            }
         }
-    }
-    
-    CCArray *first = (CCArray*) array->objectAtIndex(0);
-    Sprite *fs = (Sprite*) first->objectAtIndex(0);
-    if (fs->getPosition().y<=fs->getContentSize().height) {
-        this->gameOver();
     }
     
     period += dt;
@@ -206,43 +173,43 @@ void PlayScene::initWithLevel(int level){
 void PlayScene::blockClick(int tag[],int len){
 //    MenuItemImage *item = (MenuItemImage*)pSender;
 //    int index = item->getTag();
-    int rmtag = 0;
-    CCArray *first = (CCArray*) array->objectAtIndex(0);
-    for (int ii = 0; ii<len; ii++) {
-        int t = tag[ii];
-        Sprite *fs = (Sprite*) first->objectAtIndex(t);
-        if (fs->getParent() == nullptr) {
-            this->gameOver();
-            return;
+    CCArray *firstafterrm = CCArray::createWithCapacity(lineCount);
+    CCArray *first = CCArray::createWithCapacity(lineCount);
+    for (int i = 0 ; i<len; i++) {
+        int value = tag[i];
+        CCArray *line = (CCArray*)array->objectAtIndex(i);
+        Sprite *fb = (Sprite*)line->objectAtIndex(0);
+        if (value == 1) {
+            first->addObject(fb);
+            line->removeObjectAtIndex(0);
         }else{
-            score++;
-            fs->removeFromParent();
-            rmtag = fs->getTag();
+            firstafterrm->addObject(fb);
         }
-        for (int j = ii+1; j<len; j++) {
-            int tt = tag[j];
-            Sprite *ofs = (Sprite*) first->objectAtIndex(tt);
+    }
+    
+    int rmtag = 0;
+    for (int ii = 0; ii<first->count(); ii++) {
+        Sprite *fs = (Sprite*) first->objectAtIndex(ii);
+        
+        for (int j = ii+1; j<first->count(); j++) {
+            Sprite *ofs = (Sprite*) first->objectAtIndex(j);
             if (fs->getTag() != ofs->getTag()) {
                 this->gameOver();
                 return;
             }
         }
+        
+        score++;
+        fs->removeFromParent();
+        rmtag = fs->getTag();
     }
-    
-    bool empty = true;
-    for (int i = 0 ; i<first->count(); i++) {
-        Sprite *s = (Sprite*) first->objectAtIndex(i);
-        if (s->getParent()!=nullptr) {
-            empty = false;
-            
-            if (s->getTag()==rmtag) {
-                this->gameOver();
-                return;
-            }
+
+    for (int i = 0 ; i<firstafterrm->count(); i++) {
+        Sprite *s = (Sprite*) firstafterrm->objectAtIndex(i);
+        if (s->getTag()==rmtag) {
+            this->gameOver();
+            return;
         }
-    }
-    if (empty) {
-        array->removeObjectAtIndex(0);
     }
     
     char scc[10];
@@ -252,9 +219,8 @@ void PlayScene::blockClick(int tag[],int len){
 }
 
 void PlayScene::clickline(int line,int state){
-     CCArray *first = (CCArray*) array->objectAtIndex(0);
-    Sprite *sp = (Sprite*)first->objectAtIndex(line);
-    
+    CCArray *first = (CCArray*) array->objectAtIndex(line);
+    Sprite *sp = (Sprite*)first->objectAtIndex(0);
     if (state == 1) {
         ScaleTo *stos = ScaleTo::create(0.02, 0.9);
         ScaleTo *stob = ScaleTo::create(0.1, 1.5);
@@ -270,8 +236,11 @@ void PlayScene::clickline(int line,int state){
 
 void PlayScene::start_play(){
     
-    array = CCArray::create();
+    array = CCArray::createWithCapacity(lineCount);
     array->retain();
+    for (int i =0; i<lineCount; i++) {
+        array->addObject(CCArray::create());
+    }
     
     this->genBlockLine();
     
@@ -282,20 +251,15 @@ void PlayScene::start_play(){
         return true;
     };
     ls->onTouchEnded = [this](Touch *touch,Event *event){
-//        if(this->firsttouch){
-//            this->firsttouch = false;
-//            return;
-//        }
-//        int touchTag[touches.size()];
-//        memset(touchTag, -1, touches.size());
-//        for (int i = 0; i<touches.size(); i++) {
-//            Touch *touch = touches.at(i);
+
         int tag = (int)(touch->getLocation().x / linewidth);
+        CCArray *first = (CCArray*) array->objectAtIndex(tag);
+        if (first->count() == 0) {
+            return;
+        }
+        
         touchrecord[tag] *= -1;
         this->clickline(tag,touchrecord[tag]);
-//            touchTag[i]=tag;
-//        }
-//        this->blockClick(touchTag,touches.size());
     };
     Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(ls, this);
 }
