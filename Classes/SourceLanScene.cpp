b7887c0a20d9429536b3092ff77a0b367023d66c
Luckyhu <huji0624@gmail.com>
2014-07-22 00:09:09 +0800

    add some layer.
    FYI.

--- Classes/SourceLanScene.cpp
+++ Classes/SourceLanScene.cpp
@@ -1,61 +1,41 @@
-#include "HelloWorldScene.h"
-#include "PlayScene.h"
-#include "UMShareButton.h"
-#include "ThirdPartyHelper.h"
+//
+//  SourceLanScene.cpp
+//  LHCocosFirstGame
+//
+//  Created by huji on 14-7-21.
+//
+//
+
+#include "SourceLanScene.h"
 #include "UIListView.h"
 #include "UIText.h"
+#include "cocostudio/CocoStudio.h"
 
 USING_NS_CC;
-USING_NS_UM_SOCIAL;
 
-Scene* HelloWorld::createScene()
+Scene* SourceLanScene::createScene(cocos2d::CCString* learn)
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
     
     // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
+    auto layer = SourceLanScene::create();
+    layer->initLearn(learn);
     
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
 
-static void shareCallback(int platform, int stCode, string& errorMsg)
-{
-    if ( stCode == 100 )
-    {
-        CCLog("#### HelloWorld 开始分享");
-    }
-    else if ( stCode == 200 )
-    {
-        CCLog("#### HelloWorld 分享成功");
-    }
-    else
-    {
-        CCLog("#### HelloWorld 分享出错");
-    }
-    
-    CCLog("platform num is : %d.", platform);
-}
-
-// on "init" you need to initialize your instance
-bool HelloWorld::init()
-{
-    //////////////////////////////
-    // 1. super init first
-    if ( !Layer::init() )
-    {
-        return false;
-    }
-    
+bool SourceLanScene::initLearn(cocos2d::CCString* learn){
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    _learns.pushBack(cocos2d::CCString::create("German"));
-    _learns.pushBack(cocos2d::CCString::create("English"));
+    
+    std::stringstream ss;
+    ss<<learn->getCString()<<".json";
+    std::string s = cocos2d::FileUtils::getInstance()->getStringFromFile(ss.str());
     
     auto list = cocos2d::ui::ListView::create();
     list->setSize(Size(visibleSize.width/3*2,visibleSize.height/5*4));
@@ -63,8 +43,8 @@ bool HelloWorld::init()
     list->cocos2d::Node::setPosition(visibleSize.width/2 - ls.width/2, visibleSize.height/2 - ls.height/2);
     this->addChild(list);
     
-    for (int i = 0 ; i < _learns.size(); i++) {
-        cocos2d::CCString *s = _learns.at(i);
+    for (int i = 0 ; i < _sources.size(); i++) {
+        cocos2d::CCString *s = _sources.at(i);
         cocos2d::ui::Text *text = cocos2d::ui::Text::create(s->getCString(), "Arial", 60);
         text->setTextHorizontalAlignment(cocos2d::TextHAlignment::CENTER);
         list->pushBackCustomItem(text);
@@ -73,11 +53,10 @@ bool HelloWorld::init()
     list->addEventListener([this](Ref* p, cocos2d::ui::ListView::EventType t){
         if (t == cocos2d::ui::ListView::EventType::ON_SELECTED_ITEM_END) {
             cocos2d::ui::ListView *lv =(cocos2d::ui::ListView*) p;
-            cocos2d::CCString *se = _learns.at(lv->getCurSelectedIndex());
+            cocos2d::CCString *se = _sources.at(lv->getCurSelectedIndex());
             
         }
     });
     
     return true;
 }
-
---
+++ Classes/SourceLanScene.h
@@ -0,0 +1,30 @@
+//
+//  SourceLanScene.h
+//  LHCocosFirstGame
+//
+//  Created by huji on 14-7-21.
+//
+//
+
+#ifndef __LHCocosFirstGame__SourceLanScene__
+#define __LHCocosFirstGame__SourceLanScene__
+
+#include <iostream>
+#include "cocos2d.h"
+
+class SourceLanScene : public cocos2d::Layer{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene(cocos2d::CCString* learn);
+    
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    bool initLearn(cocos2d::CCString* learn);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(SourceLanScene);
+    
+private:
+    cocos2d::Vector<cocos2d::CCString*> _sources;
+};
+
+#endif /* defined(__LHCocosFirstGame__SourceLanScene__) */
