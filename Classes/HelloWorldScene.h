4ad7c1eb2af6bf29eb3602e64b10d6aa175a6bf1
Luckyhu <huji0624@gmail.com>
2014-07-22 00:09:09 +0800

    add some layer.
    FYI.

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -13,11 +13,11 @@ public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();  
     
-    // a selector callback
-    void testClick(cocos2d::Ref* pSender);
-    
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+    
+private:
+    cocos2d::Vector<cocos2d::CCString*> _learns;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
