45725eb5a7eac82209c5f4db5e20fe1ede05eea5
Luckyhu <huji0624@gmail.com>
2014-07-22 14:31:17 +0800

    add some tool.
    FYI.

---
+++ tools/sceneGen.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+
+name = sys.argv[1]
+upname = name.upper()
+
+print "//------.h"
+print "#ifndef __%s__" % (upname)
+print "#define __%s__" % (upname)
+print "#include \"cocos2d.h\""
+print "class %s : public cocos2d::Layer" % (name)
+print "{"
+print "public:"
+print "	static cocos2d::Scene* createScene(cocos2d::CCDictionary *dic);"
+print "	bool initDict(cocos2d::CCDictionary *dic);"
+print "	CREATE_FUNC(%s);" % (name)
+print "};"
+print "#endif"
+
+print "//------.cpp"
+print "#include \"%s.h\"" % (name)
+print "USING_NS_CC;"
+print "Scene* %s::createScene(cocos2d::CCDictionary *dic)" % (name)
+print "{"
+print "	auto scene = Scene::create();"
+print "	auto layer = %s::create();" % (name)
+print "	layer->initDict(dic);"
+print "	scene->addChild(layer);"
+print "	return scene;"
+print "}\n"
+print "bool %s::initDict(cocos2d::CCDictionary *dic)" % (name)
+print "{\n"
+print "\treturn true;\n}"
