732f0090c161c8bd39f59b838218e8b10fac2bcb
Luckyhu <huji0624@gmail.com>
2014-09-22 21:11:04 +0800

    fix icon and image issue.
    fyi.

--- tools/imageMaker.py
+++ tools/imageMaker.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 if len(sys.argv)==1:
-	print "python imageMaker.py config sourceimage"
+	print "python imageMaker.py config sourceimage [--ignorename]"
 	exit(1)
 
 confpath = sys.argv[1]
@@ -20,5 +20,10 @@ for l in lines:
 	outname = confs[0].strip()
 	width = confs[1].strip()
 	height =confs[2].strip()
-	command = 'sips -z %s %s %s --out %s' % (width,height,source,outname)
+	if '--ignorename' in sys.argv:
+		basename = os.path.basename(source)
+		prename = basename.split('.')[0]
+		command = 'sips -z %s %s %s --out %s_%s_%sx%s.png' % (height,width,source,prename,outname,width,height)
+	else:
+		command = 'sips -z %s %s %s --out %s' % (height,width,source,outname)
 	rt = os.system(command)
