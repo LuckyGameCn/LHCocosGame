7a661a47ce3662de50cb8837917519016577ac29
Luckyhu <huji0624@gmail.com>
2014-12-18 19:13:29 +0800

    add pomelo.
    fyi.

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../..)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -17,16 +18,6 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 ../../Classes/DeveloperInfoScence.cpp \
 ../../Classes/GameOverScene.cpp \
 ../../Classes/HelloWorldScene.cpp \
-../../Classes/jansson/dump.c \
-../../Classes/jansson/error.c \
-../../Classes/jansson/hashtable.c \
-../../Classes/jansson/load.c \
-../../Classes/jansson/memory.c \
-../../Classes/jansson/pack_unpack.c \
-../../Classes/jansson/strbuffer.c \
-../../Classes/jansson/strconv.c \
-../../Classes/jansson/utf.c \
-../../Classes/jansson/value.c \
 ../../Classes/LHLeaderBoard.cpp \
 ../../Classes/LHPauseScene.cpp \
 ../../Classes/LHShareButton.cpp \
@@ -35,16 +26,18 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 ../../Classes/NDKHelper/NDKHelper.cpp \
 ../../Classes/PlayScene.cpp \
 ../../Classes/ThirdPartyHelper.cpp \
+../../Classes/LHGameLayer.cpp \
 ../../Classes/UMShareButton.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
 					$(LOCAL_PATH)/../../Classes/NDKHelper \
-					$(LOCAL_PATH)/../../Classes/jansson \
+					$(LOCAL_PATH)/../../libpomelo \
 					$(LOCAL_PATH)/../../cocos2d/cocos/ui
 
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += pomelo_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_ui_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 # LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
@@ -57,9 +50,5 @@ include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
 $(call import-module,audio/android)
-# $(call import-module,ui)
-# $(call import-module,Box2D)
-# $(call import-module,editor-support/cocosbuilder)
-# $(call import-module,editor-support/spine)
 $(call import-module,editor-support/cocostudio)
-# $(call import-module,network)
+$(call import-module,libpomelo)
