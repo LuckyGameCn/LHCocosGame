67595fdae5289b8e6f4f527fb04f4df42e8494b8
huji <huji@didichuxing.com>
2016-02-22 18:28:13 +0800

    fix compile error.

--- proj.ios_mac/ios/main.m
+++ proj.ios_mac/ios/main.m
@@ -8,15 +8,34 @@ int main(int argc, char *argv[]) {
     return retVal;
 }
 
+#if defined (__unix) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+#ifndef __ENABLE_COMPATIBILITY_WITH_UNIX_2003__
+#define __ENABLE_COMPATIBILITY_WITH_UNIX_2003__
 #include <stdio.h>
-//FILE *fopen$UNIX2003( const char *filename, const char *mode )
-//{
-//    return fopen(filename, mode);
-//}
+#include <dirent.h>
+FILE *fopen$UNIX2003( const char *filename, const char *mode )
+{
+    return fopen(filename, mode);
+}
 size_t fwrite$UNIX2003( const void *a, size_t b, size_t c, FILE *d )
 {
-	return fwrite(a, b, c, d);
+    return fwrite(a, b, c, d);
 }
-int strerror$UNIX2003(int i){
-	return strerror(i);
+char *strerror$UNIX2003( int errnum )
+{
+    
+    return strerror(errnum);
 }
+
+DIR *opendir$INODE64(const char * a)
+{
+    return opendir(a);
+}
+
+struct dirent *readdir$INODE64(DIR *dir)
+{
+    return readdir(dir);
+}
+
+#endif
+#endif
